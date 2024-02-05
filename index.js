9fee24b43d4461cd33f4fd499cc8e7e32bf9169f
muhammadniazi123 <126714959+muhammadniazi123@users.noreply.github.com>
2024-02-05 16:35:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,19 +22,6 @@ app.get("/", (req, res) => {
   res.redirect('/');
     });
 
-
-    app.get("/work", (req, res) => {
-      var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      var today  = new Date();
-      let day=today.toLocaleDateString("en-US", options);
-      res.render("work.ejs",{kindofday: day, newlistitems: newitems}); 
-    });
-    app.post("/work", (req, res) => {
-      let newitem=req.body.newitem;
-      newitems.push(newitem)
-      res.redirect('/work');
-        });
-
 app.listen(port,()=>{
     console.log(`This site is running on port ${port}`)
 })
